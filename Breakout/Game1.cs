8203baa4eb071028e5a2210fd20320c3432850f6
Heritiana Luca TOBE <123lucatobe@gmail.com>
2024-08-28 12:38:27 +0300

    Easy AABB

--- Breakout/Game1.cs
+++ Breakout/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Screens;
 
 namespace Breakout
 {
@@ -8,7 +10,11 @@ namespace Breakout
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private bool _isColliding = false;
+        private RectangleF _rectA;
+        private RectangleF _rectB;
+        private float _speed = 200.0f;
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,7 +25,8 @@ namespace Breakout
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _rectA = new Rectangle(100, 10, 50, 30);
+            _rectB = new Rectangle(100, 100, 200, 100);
             base.Initialize();
         }
 
@@ -36,15 +43,56 @@ namespace Breakout
                 Exit();
 
             // TODO: Add your update logic here
+            var kbState = Keyboard.GetState();
 
+            if (kbState.IsKeyDown(Keys.Up)) 
+            {
+                _rectA.Y -= _speed * gameTime.GetElapsedSeconds();
+            }
+
+            if (kbState.IsKeyDown(Keys.Down))
+            {
+                _rectA.Y += _speed * gameTime.GetElapsedSeconds();
+            }
+
+            if (kbState.IsKeyDown(Keys.Left))
+            {
+                _rectA.X -= _speed * gameTime.GetElapsedSeconds();
+            }
+
+            if (kbState.IsKeyDown(Keys.Right)) 
+            {
+                _rectA.X += _speed * gameTime.GetElapsedSeconds();
+            }
+
+            if (_rectA.Y <= 0) 
+                _rectA.Y = 0;
+            if (_rectA.Bottom >= _graphics.PreferredBackBufferHeight)
+                _rectA.Y = _graphics.PreferredBackBufferHeight - _rectA.Height;
+
+            if (_rectA.X <= 0)
+                _rectA.X = 0;
+            if (_rectA.Right >= _graphics.PreferredBackBufferWidth)
+                _rectA.X = _graphics.PreferredBackBufferWidth - _rectA.Width;
+
+            if (_rectA.Intersects(_rectB))
+                _isColliding = true;
+            else
+                _isColliding = false;
+            
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.DrawRectangle(_rectA, _isColliding ? Color.Red : Color.White);
+            _spriteBatch.DrawRectangle(_rectB, _isColliding ? Color.Red : Color.White);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
